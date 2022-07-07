778e4d917a6415e344dc039c5690222370a72f38
qyoubgamer <qyoubgamer@gmail.com>
2022-07-07 02:27:03 +0200

    first commit

---
+++ spotify_v2/src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AUTH_URL =
+  'https://accounts.spotify.com/authorize?client_id=947be400c5ff45ce9211f8370de87cc3&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+
+export default function login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: '100vh' }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
+}
